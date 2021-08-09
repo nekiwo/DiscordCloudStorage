a79adaf16cce34c33f5dc5508dda71c5bb2d1db6
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-08-09 00:16:13 -0500

    a

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,16 @@
-let upload = () => {
+const upload = () => {
     const file = document.getElementById("input").files[0];
-}
+    const FormData = new FormData();
+    FormData.append("file", file);
+
+    fetch("/upload", {
+        method: "POST",
+        body: FormData
+    }).then(
+        response => response.json()
+    ).then(
+        data => {
+            console.log(data);
+        }
+    ).catch(error => console.log(error));
+};
--- src/server.go
+++ src/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func server() {
@@ -10,12 +13,24 @@ func server() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
 	// Routing
-	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/upload", func(res http.ResponseWriter, req *http.Request) {
+		req.MultipartReader()
+
+		file, handler, err := req.FormFile("file")
+		if err != nil {fmt.Println(err)}
+		defer file.Close()
+
+		// Create file
+		dst, err := os.Create("./bin/" + handler.Filename)
+		defer dst.Close()
+		if err != nil {http.Error(res, err.Error(), http.StatusInternalServerError)}
+
+		// Copy over the file
+		if _, err := io.Copy(dst, file); err != nil {http.Error(res, err.Error(), http.StatusInternalServerError)}
+
+		//https://socketloop.com/tutorials/golang-how-to-split-or-chunking-a-file-to-smaller-pieces
+	})
 
 	// Server stuff
 	log.Fatal(http.ListenAndServe(":3000", nil))
-}
-
-func upload(res http.ResponseWriter, req *http.Request) {
-
 }
