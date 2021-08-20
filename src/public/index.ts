35749ef9fa2dc4b9d6540a2c7a6bfb5d9e488936
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-08-19 21:01:01 -0500

    a

--- src/public/index.ts
+++ src/public/index.ts
@@ -5,29 +5,36 @@ const LinkBtn: HTMLElement = document.getElementById("linkbtn")
 const upload: Function = () => {
     const input: HTMLInputElement = <HTMLInputElement>document.getElementById("input")
     const file: File = input.files[0];
-    const form: FormData = new FormData();
-    form.append("file", file);
 
-    fetch("/upload", {
-        method: "POST",
-        body: form
-    }).then((data: Response) => {
-        console.log(data)
-        data.text().then((url: string) => {
-            console.log(url)
-            DownloadURL = url
+    if (input.value != "") {
+        const form: FormData = new FormData();
+        form.append("file", file);
 
-            LinkBtn.style.display = "block";
+        document.getElementById("upload").innerHTML = "Uploading..."
+
+        fetch("/upload", {
+            method: "POST",
+            body: form
+        }).then((data: Response) => {
+            console.log(data)
+            data.text().then((url: string) => {
+                console.log(url)
+                DownloadURL = url
+
+                document.getElementById("upload").innerHTML = "Uploaded!"
+                document.getElementById("uploadbtn").style.display = "none"
+                LinkBtn.style.display = "block"
+            })
+        }).catch((err: Error) => {
+            console.log(err)
+            alert("Error: " + err)
         })
-    }).catch((err: Error) => {
-        console.log(err)
-        alert("Error: " + err)
-    })
+    }
 }
 
 const CopyLink: Function = () => {
     if (DownloadURL != "null") {
-        navigator.clipboard.writeText(DownloadURL).then(() => {
+        navigator.clipboard.writeText(document.location.origin + DownloadURL).then(() => {
             LinkBtn.innerHTML = "Link copied!"
             setTimeout(() => {
                 LinkBtn.innerHTML = "Copy link"
